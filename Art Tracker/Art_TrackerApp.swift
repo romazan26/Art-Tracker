ef14072fe0346e4d7301e2850f75555f89b0c5d8
romazan26 <130604263+romazan26@users.noreply.github.com>
2024-10-01 15:23:32 +0300

    Initial Commit

---
+++ Art Tracker/Art_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Art_TrackerApp.swift
+//  Art Tracker
+//
+//  Created by Роман on 01.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct Art_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
