dc8cfb8bd01ba1127dc641eec3eef5e9efd99e36
romazan26 <130604263+romazan26@users.noreply.github.com>
2024-10-07 19:21:02 +0300

    pre relise

---
+++ Art Tracker/Views/Notes/NoteView.swift
@@ -0,0 +1,18 @@
+//
+//  NoteView.swift
+//  Art Tracker
+//
+//  Created by Роман on 07.10.2024.
+//
+
+import SwiftUI
+
+struct NoteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoteView()
+}
---
+++ Art Tracker/Views/User/UserView.swift
@@ -0,0 +1,18 @@
+//
+//  UserView.swift
+//  Art Tracker
+//
+//  Created by Роман on 07.10.2024.
+//
+
+import SwiftUI
+
+struct UserView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserView()
+}
